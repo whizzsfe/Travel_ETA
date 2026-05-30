4f2b204a0c3ecf464e8c099ae8b97b865bfdcbf1
whizzsfe <whizzsfe@users.noreply.github.com>
2026-05-30 05:54:19 +0000

    deploy: f9c88572186c2ff5ad90cdc86c70ec457e32520a

---
+++ js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\h\u0028\u0029",
+            "name": "h",
+            "summary": "Escape\u0020a\u0020string\u0020for\u0020safe\u0020HTML\u0020output.",
+            "url": "namespaces/default.html#function_h"
+        },                {
+            "fqsen": "\\fmtDt\u0028\u0029",
+            "name": "fmtDt",
+            "summary": "Format\u0020a\u0020UTC\/London\u0020datetime\u0020string\u0020for\u0020display.",
+            "url": "namespaces/default.html#function_fmtDt"
+        },                {
+            "fqsen": "\\call_routes_api\u0028\u0029",
+            "name": "call_routes_api",
+            "summary": "Call\u0020the\u0020Google\u0020Maps\u0020Routes\u0020API\u0020v2\u0020for\u0020a\u0020single\u0020departure\u0020slot.",
+            "url": "namespaces/default.html#function_call_routes_api"
+        },                {
+            "fqsen": "\\formatDeparture\u0028\u0029",
+            "name": "formatDeparture",
+            "summary": "Format\u0020a\u0020departure\u0020datetime\u0020string\u0020for\u0020display\u0020in\u0020flash\u0020messages\u0020and\u0020history.",
+            "url": "namespaces/default.html#function_formatDeparture"
+        },                {
+            "fqsen": "\\deltaBadge\u0028\u0029",
+            "name": "deltaBadge",
+            "summary": "Render\u0020a\u0020Bootstrap\u0020badge\u0020showing\u0020how\u0020early\u0020or\u0020late\u0020an\u0020arrival\u0020is.",
+            "url": "namespaces/default.html#function_deltaBadge"
+        },                {
+            "fqsen": "\\freshnessBanner\u0028\u0029",
+            "name": "freshnessBanner",
+            "summary": "Render\u0020a\u0020traffic\u002Dfreshness\u0020alert\u0020banner\u0020based\u0020on\u0020how\u0020far\u0020away\u0020the\u0020best\u0020departure\u0020is.",
+            "url": "namespaces/default.html#function_freshnessBanner"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
